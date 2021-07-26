b9e43b8cc4f0e8e76a3e5871da5280421cce8be2
Feirly Moore <59799408+FeirlyMoore@users.noreply.github.com>
2021-07-27 00:47:05 +0300

    Add files via upload

---
+++ REDFOX39 inc/JavaScript/indexjs.js
@@ -0,0 +1,19 @@
+console.log('%cТы шо, Вася, потерялася ? 👿', 'font: 5em roboto; color: #e95420;');
+
+//window.onscroll = function() {ScrollNav()};
+
+/*function ScrollNav() {
+  if (document.body.scrollDown > 50 || document.documentElement.scrollTop > 50) 
+  {
+    document.querySelector('.nav_block').className = "mobile-menu2";
+  } else {
+    document.querySelector('.nav_block').className = "mobile-menu";
+  }
+}*/
+
+   /* for (let a = 6; 5 < a; a++)
+    {
+       b = a++;
+       let g = b * a;
+       console.log(g);
+    }*/
